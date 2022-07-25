432a4698f616121061b4f7760611d783e05b5d73
Oh haengsong <haengsong@naver.com>
2022-07-25 17:02:29 +0900

    merge test

---
+++ Back-end/src/main/java/com/ssafy/soldsolve/SoldSolveApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.soldsolve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoldSolveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoldSolveApplication.class, args);
+	}
+
+}

2ae404e23801e7e626ddd2caadad5f551f916dfa
SSAFY <myuju419@naver.com>
2022-07-25 14:04:21 +0900

    MyPage update

--- Front-end/src/routers/MyPage.js
+++ Front-end/src/routers/MyPage.js
@@ -1,36 +1,73 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavBar from '../components/NavBar'
+import Modal from '../components/Modals/Modal';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartArrowDown, faReceipt } from '@fortawesome/free-solid-svg-icons'
 
-
+import './routers.css';
 
 function MyPage() {
+
+    const [modalOpen, setModalOpen] = useState(false);
+
+    const openModal = () => {
+        setModalOpen(true);
+    };
+    const closeModal = () => {
+        setModalOpen(false);
+    };
+
+
     return (
-        <div>
+        <>
             <NavBar />
-            <body>
-
-                <div class="container">
-                    <div class="content">
-
-                        <nav role="navigation">
-                            <div id="menuToggle">
-                                <input type="checkbox" />
-                                <span></span>
-                                <span></span>
-                                <span></span>
-                                <ul id="menu">
-                                    <li><a href="#!">Home</a></li>
-                                    <li><a href="#!">About</a></li>
-                                    <li><a href="#!">Info</a></li>
-                                    <li><a href="#!">Contact</a></li>
-                                </ul>
-                            </div>
-                        </nav>
+            <div className='mypage' style={{ margin: 30 }}>
+                <h3>MY PAGE</h3>
+                <div className='account_container'>
+                    <div className='column'>사진자리</div>
+                    <div className='column'>
+                        <div className=''>이름자리</div>
+                        <div className=''>회원정보 수정 자리</div>
+                        
                     </div>
                 </div>
+                <div className='history_container'>
+                    <div className='column'>
+                        <button className='dot' onClick={openModal}>
+                        <FontAwesomeIcon className='icon' icon={faCartArrowDown} size="2x" />
+                        <div>구매내역</div>
+                        </button>
+                        <Modal open={modalOpen} close={closeModal} header="Modal heading">
+                            구매내역리스트
+                        </Modal>
+                    </div>
+                    <div className='column'>
+                        <button className='dot' onClick={openModal}>
+                        <FontAwesomeIcon className='icon' icon={faReceipt} size="2x" />
+                        <div>판매내역</div>
+                        </button>
+                        <Modal open={modalOpen} close={closeModal} header="Modal heading">
+                            판매내역리스트
+                        </Modal>
+                    </div>
+                </div>
+                <hr />
+                <div className='heart_container'>
+                    <div className='column'>
+                        찜한상품
+                    </div>
+                    <div className='column'>
+                        더보기
+                    </div>
+                </div>
+                <div>
+                    상품 6개만 나오게
+                    없으면 없다 표시
+                </div>
+                <hr />
+            </div>
 
-            </body>
-        </div>
+        </>
     )
 }
 

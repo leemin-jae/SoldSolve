2a815502b6cf8ddcb57b79922f12cac6965d36d5
조영훈 <gcloud1025@gmail.com>
2022-07-31 20:49:42 +0900

    notice paging

--- Back-end/src/main/java/com/ssafy/soldsolve/api/response/MessageRes.java
+++ Back-end/src/main/java/com/ssafy/soldsolve/api/response/MessageRes.java
@@ -1,19 +1,24 @@
 package com.ssafy.soldsolve.api.response;
 
 import com.ssafy.soldsolve.db.entity.Message;
+import com.ssafy.soldsolve.db.entity.User;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.ManyToOne;
+
 @Getter @Setter
 public class MessageRes {
-    String title;
     String content;
     Boolean isRead;
+    @ManyToOne
+    User user;
 
     public static MessageRes of(Message message) {
         MessageRes res = new MessageRes();
         res.setContent(message.getContent());
         res.setIsRead(message.getIsRead());
+        res.setUser(message.getUser());
         return res;
     }
 }
--- Back-end/src/main/java/com/ssafy/soldsolve/api/service/NoticeService.java
+++ Back-end/src/main/java/com/ssafy/soldsolve/api/service/NoticeService.java
@@ -2,8 +2,10 @@ package com.ssafy.soldsolve.api.service;
 
 import com.ssafy.soldsolve.api.request.NoticePostReq;
 import com.ssafy.soldsolve.db.entity.Notice;
+import org.springframework.data.domain.Page;
 
 public interface NoticeService {
+    Page<Notice> findAll(int page);
     Notice createNotice(NoticePostReq registerInfo);
     Notice getNotice(int noticeId);
     void updateNotice(NoticePostReq noticePostReq, Notice notice);

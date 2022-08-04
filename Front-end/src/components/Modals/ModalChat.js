561f4c401c3f975c6af271ff64248b25420306ae
이건후 <lgh3806@naver.com>
2022-08-04 18:12:37 +0900

    chat modal UI update

--- Front-end/src/components/Modals/ModalChat.js
+++ Front-end/src/components/Modals/ModalChat.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './modal.css';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPaperPlane, faChevronLeft } from '@fortawesome/free-solid-svg-icons'
-import '../../routers/routers.css'
+// import '../../routers/routers.css'
 // import { useNavigate } from 'react-router-dom';
 
 
@@ -10,7 +10,7 @@ const ModalChat = (props) => {
   // 열기, 닫기, 모달 헤더 텍스트를 부모로부터 받아옴
   // let navigate = useNavigate()
 
-  const { open, close } = props;
+  const { open, close, header } = props;
 
   return (
     // 모달이 열릴때 openModal 클래스가 생성된다.
@@ -20,8 +20,7 @@ const ModalChat = (props) => {
           <main>
             <div className='chat_box'>
               <FontAwesomeIcon className='buyer_nickname' icon={faChevronLeft} style={{ float: 'right', width: '28px', height: '28px', margin: '4px 2px 0 8px', color: '#6667AB', marginRight: '265px', marginBottom: '12px' }} onClick={close} />
-              <div className='buyer_nickname'>상대방 닉네임</div>
-              <div className='buyer_nickname'>상대방 닉네임</div>
+              <h3 className='buyer_nickname'>{header}</h3>
               <div className='chat_background'></div>
               <div className='input_box'>
                 <input className='chat_input' type="text" />
--- Front-end/src/components/Notice/Chat.js
+++ Front-end/src/components/Notice/Chat.js
@@ -3,15 +3,20 @@ import { useState } from "react"
 // import ChatRoom from '../Modals/ChatRoom'
 // import { useNavigate } from 'react-router-dom';
 import ModalChat from '../Modals/ModalChat';
+import '../components.css'
+
 
 function Chat() {
+  const [buyerName, setBuyerName] = useState('')
 
   // let navigate = useNavigate()
   const [modalOpen, setModalOpen] = useState(false);
 
-  const openModal = () => {
+  const openModal = (buyerName) => {
+    // console.log(e)
+    setBuyerName(buyerName)
     setModalOpen(true);
-    console.log(modalOpen)
+    // console.log(buyerName)
 
   };
   const closeModal = () => {
@@ -23,7 +28,7 @@ function Chat() {
 
   const data = [
     { id: 1, buyer: 'buyer 1', thumbnail: 'https://images.mypetlife.co.kr/content/uploads/2019/09/09152804/ricky-kharawala-adK3Vu70DEQ-unsplash.jpg', text: '안녕하세유', time: '2022-08-03 pm 09:56' },
-    { id: 2, buyer: 'buyer 2', thumbnail: 'https://post-phinf.pstatic.net/MjAyMDAyMjJfMSAg/MDAxNTgyMzY1NzE3MzEw.GsKJMsvSf2CnkhZQ4eTSGD8m3DS5QLUJNKPs3P97vW0g.ca3xreRCcA2dmsA73cDuVU8c15vaQaPbjANR-ykoYDog.JPEG/%ED%96%84%EC%8A%A4%ED%84%B05.jpg?type=w1200', text: 'Zzz...', time: '2022-08-03 pm 09:56' },
+    { id: 2, buyer: 'buyer 2', thumbnail: 'https://post-phinf.pstatic.net/MjAyMDAyMjJfMSAg/MDAxNTgyMzY1NzE3MzEw.GsKJMsvSf2CnkhZQ4eTSGD8m3DS5QLUJNKPs3P97vW0g.ca3xreRCcA2dmsA73cDuVU8c15vaQaPbjANR-ykoYDog.JPEG/%ED%96%84%EC%8A%A4%ED%84%B05.jpg?type=w1200', text: '가나다라', time: '2022-08-03 pm 09:56' },
     { id: 3, buyer: 'buyer 3', thumbnail: 'https://pds.joongang.co.kr/news/FbMetaImage/202203/96548fe5-a590-41ea-9233-262ee6774a4f.png', text: '반갑습니다', time: '2022-08-03 pm 09:56' },
     { id: 4, buyer: 'buyer 4', thumbnail: 'https://img.seoul.co.kr/img/upload/2021/05/03/SSI_20210503113234_O2.jpg', text: '하이하이', time: '2022-08-03 pm 09:56' },
     { id: 5, buyer: 'buyer 5', thumbnail: 'https://blog.kakaocdn.net/dn/5hzaw/btqDqiXDHo6/gtyyKVxoQbDgzJuzMMzDYk/img.jpg', text: '졸립니다', time: '2022-08-03 pm 09:56' },
@@ -31,37 +36,60 @@ function Chat() {
   const [roomLists] = useState(data)
   const [userName] = useState('hoho')
 
+  const ShowChat = () => {
+    // filterProduct("women's clothing")
+    console.log(buyerName)
+    return (
+      <>
+        {roomLists.map((roomList) => {
+          return (
+
+            <li className='chat_room' key={roomList.id} style={{ cursor: 'pointer' }} >
+
+
+              <div style={{ display: 'flex', alignItems: 'center' }} onClick={() => {
+                // setBuyerName()
+                openModal(roomList.buyer)
+
+              }}>
+                <div className="profile_box" style={{ background: '#BDBDBD' }}>
+                  <img className="profile_img" src={roomList.thumbnail} alt='profileImg' />
+                </div>
+
+                <div>
+                  <h6 className='profile_text'>{roomList.buyer}</h6>
+                  <div className='profile_text'>{roomList.text}</div>
+                </div>
+              </div>
+              <div>
+                <div className='message_info'>
+                  <p className='message_time'>{roomList.time}</p>
+
+                  <div className='not_read_message'>
+                    <p>2</p>
+                  </div>
+                </div>
+              </div>
+
+
+              <ModalChat open={modalOpen} close={closeModal} header={buyerName}>
+              </ModalChat>
+            </li>
+
+
+          );
+        })}
+      </>
+    );
+
+  };
+
+
   return (
     <>
       <h2 style={{ textAlign: 'center', marginTop: '40px' }}>{userName}의 채팅방</h2>
       <ul>
-        {roomLists.map((roomList) =>
-          // 버튼 클릭 시 방 세션으로 넘어가기 (A-1, A-2, A-3, ...) 만약 없으면 새로운 방 생성?
-          <li className='chat_room' key={roomList.id} style={{ cursor: 'pointer' }} onClick={openModal}>
-            <ModalChat open={modalOpen} close={closeModal}>
-            </ModalChat>
-            <div style={{ display: 'flex', alignItems: 'center' }}>
-              <div className="profile_box" style={{ background: '#BDBDBD' }}>
-                <img className="profile_img" src={roomList.thumbnail} alt='profileImg' />
-              </div>
-
-              <div>
-                <h6 className='profile_text'>{roomList.buyer}</h6>
-                <p className='profile_text'>{roomList.text}</p>
-              </div>
-            </div>
-            <div>
-              <div className='message_info'>
-                <p>{roomList.time}</p>
-
-                <div className='not_read_message'>
-                  <p style={{ color: 'white', marginLeft: 'auto', textAlign: 'center' }}>2</p>
-                </div>
-              </div>
-            </div>
-
-          </li>
-        )}
+        {<ShowChat />}
       </ul>
     </>
   );

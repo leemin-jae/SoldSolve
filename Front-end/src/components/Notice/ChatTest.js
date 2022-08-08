b3fdd455f6eb9b212b1074d7618150b48db87b90
이건후 <lgh3806@naver.com>
2022-08-09 00:59:08 +0900

    FE: Chat test update

--- Front-end/src/components/Notice/ChatTest.js
+++ Front-end/src/components/Notice/ChatTest.js
@@ -2,20 +2,10 @@ import React, { useEffect, useState } from 'react';
 import { over } from 'stompjs'
 import SockJS from 'sockjs-client';
 import axios from 'axios'
-import './modal.css';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPaperPlane, faChevronLeft } from '@fortawesome/free-solid-svg-icons'
-// import '../../routers/routers.css'
-// import { useNavigate } from 'react-router-dom';
 
 let stompClient = null
 
-const ModalChat = (props) => {
-  // 열기, 닫기, 모달 헤더 텍스트를 부모로부터 받아옴
-  // let navigate = useNavigate()
-
-  const { open, close, header } = props;
-
+function ChatTest() {
   const [chats, setChats] = useState([])
   // const [privateChats, setPrivateChats] = useState(new Map())
   // const [tab, setTab] = useState("CHAT ROOM")
@@ -23,15 +13,20 @@ const ModalChat = (props) => {
     username: '',
     receivername: '',
     connected: false,
-    message: ''
+    message: '',
+    roomId: 1
   })
   const onConnected = () => {
     // console.log(payload)
     setUserData({ ...userData, "": true })
     console.log('???')
     console.log(stompClient)
-    stompClient.subscribe('/sub/chat/room/1', (payload) => {
+    console.log(typeof (1))
+    console.log(`/sub/chat/room/${2}`)
+    stompClient.subscribe(`/sub/chat/room/${2}`, (payload) => {
+      console.log(payload, '!@@@@@@')
       let payloadData = JSON.parse(payload.body);
+      console.log(payloadData, '!@#!@#')
       chats.push(payloadData);
       setChats([...chats]);
       console.log(chats)
@@ -71,46 +66,30 @@ const ModalChat = (props) => {
       let chatMessage = {
         senderName: userData.username,
         message: userData.message,
-        roomId: '1',
+        roomId: 1,
         status: 'MESSAGE'
       }
       stompClient.send('/pub/chat/message/', {}, JSON.stringify(chatMessage))
       setUserData({ ...userData, "message": "" })
-      console.log(userData)
+      console.log(stompClient)
+      console.log(userData, '123')
     }
   }
-
   return (
-    // 모달이 열릴때 openModal 클래스가 생성된다.
-    <div className={open ? 'openModal modal' : 'modal'}>
-      {open ? (
-        <section>
-          <main>
-            <div className='chat_box'>
-              <FontAwesomeIcon className='buyer_nickname' icon={faChevronLeft} style={{ float: 'right', width: '28px', height: '28px', margin: '4px 2px 0 8px', color: '#6667AB', marginRight: '265px', marginBottom: '12px', left: '6px', top: '11px' }} onClick={close} />
-              <h3 className='buyer_nickname'>{header}</h3>
-              <div className='chat_background'>
-                <div className='chat_div' >
-                  <ul className='li_box_container'>
-                    {chats.map(chat => {
-                      return (
-                        <span className='li_box_me' key={userData.userId}>{chat.message}</span>
-                      )
-                    })}
-                  </ul>
-                </div>
-              </div>
-            </div>
-            <div className='input_box'>
-              <input className='chat_input' type="text" name="message" placeholder={'enter message'} value={userData.message} onChange={handleValue} />
-              <FontAwesomeIcon icon={faPaperPlane} style={{ float: 'right', width: '28px', height: '28px', margin: '4px 2px 0 8px', color: '#6667AB' }} onClick={sendPublicMessage} />
-            </div>
-          </main>
-        </section>
-      ) : null
-      }
-    </div >
+    <div>
+      <button onClick={getAxios}>GetName</button>
+      <h1>{userData.username}</h1>
+      <input type="text" name="message" placeholder={'enter message'} value={userData.message} onChange={handleValue} />
+      <button onClick={sendPublicMessage}> send </button>
+      <ul>
+        {chats.map(chat => {
+          return (
+            <li key={userData.userId}>{chat.message}</li>
+          )
+        })}
+      </ul>
+    </div>
   );
-};
+}
 
-export default ModalChat;
+export default ChatTest;

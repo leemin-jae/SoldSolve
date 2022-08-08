76f1c1d4fbc7c35d1c0a56faee7e43d7e28e3a68
조영훈 <gcloud1025@gmail.com>
2022-08-08 13:47:48 +0900

    BE:[FEAT] request entity & rest api

---
+++ Back-end/src/main/java/com/ssafy/soldsolve/api/controller/RequestController.java
@@ -0,0 +1,74 @@
+package com.ssafy.soldsolve.api.controller;
+
+
+import com.ssafy.soldsolve.api.service.RequestService;
+import com.ssafy.soldsolve.api.service.UserService;
+import com.ssafy.soldsolve.common.auth.SsafyUserDetails;
+import com.ssafy.soldsolve.common.model.response.BaseResponseBody;
+import com.ssafy.soldsolve.db.entity.Request;
+import com.ssafy.soldsolve.db.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/requests")
+public class RequestController {
+
+    @Autowired
+    RequestService requestService;
+
+    @Autowired
+    UserService userService;
+
+    // 로그인한 유저 라이브 요청 추가
+    @PostMapping("")
+    public ResponseEntity<? extends BaseResponseBody> createRequest(Authentication authentication, @RequestParam(name = "product") int product) {
+        try {
+            SsafyUserDetails userDetails = (SsafyUserDetails) authentication.getDetails();
+            String userId = userDetails.getUsername();
+            User user = userService.getUserByUserId(userId);
+            requestService.createRequest(user, product);
+            return ResponseEntity.status(200).body(BaseResponseBody.of(200, "Success"));
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return ResponseEntity.status(200).body(BaseResponseBody.of(400, "이미 요청된 라이브입니다."));
+
+    }
+
+    // 로그인한 유저의 라이브 요청 조회
+    @GetMapping("")
+    public ResponseEntity<?> readRequest(Authentication authentication) {
+        SsafyUserDetails userDetails = (SsafyUserDetails) authentication.getDetails();
+        String userId = userDetails.getUsername();
+        User user = userService.getUserByUserId(userId);
+        List<Request> requestList = requestService.getRequest(user);
+        return ResponseEntity.status(200).body(requestList);
+    }
+
+    // 로그인한 유저의 라이브 요청 삭제
+    @DeleteMapping("")
+    public ResponseEntity<? extends BaseResponseBody> deleteRequest(Authentication authentication, @RequestParam(name = "product") int product) {
+        SsafyUserDetails userDetails = (SsafyUserDetails) authentication.getDetails();
+        String userId = userDetails.getUsername();
+        User user = userService.getUserByUserId(userId);
+        requestService.deleteRequest(user, product);
+        return ResponseEntity.status(200).body(BaseResponseBody.of(200, "Success"));
+    }
+
+    // 라이브 요청 체크 ( 빨간색 or 빈하트 구분용 )
+    @GetMapping("/check/{product}")
+    public ResponseEntity<?> checkRequest(@PathVariable int product, Authentication authentication) {
+        SsafyUserDetails userDetails = (SsafyUserDetails) authentication.getDetails();
+        String userId = userDetails.getUsername();
+        User user = userService.getUserByUserId(userId);
+        Boolean flag = false;
+        flag = requestService.checkRequest(user, product);
+        return ResponseEntity.status(200).body(flag);
+    }
+}
+
---
+++ Back-end/src/main/java/com/ssafy/soldsolve/db/repository/RequestRepository.java
@@ -0,0 +1,17 @@
+package com.ssafy.soldsolve.db.repository;
+
+import com.ssafy.soldsolve.db.entity.Product;
+import com.ssafy.soldsolve.db.entity.Request;
+import com.ssafy.soldsolve.db.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface RequestRepository extends JpaRepository<Request, Integer> {
+    Optional<List<Request>> findAllByUser(User user);
+    int countByUserAndProduct(User user, Product product);
+    Optional<Request> findByUserAndProduct(User user, Product product);
+}

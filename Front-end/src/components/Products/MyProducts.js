13cd6a35014177ad34e861199cd1fd69c6cfbebc
오행송 <haengsong@naver.com>
2022-08-15 17:23:06 +0900

    FE : sold check fix

--- Front-end/src/components/Products/MyProducts.js
+++ Front-end/src/components/Products/MyProducts.js
@@ -8,32 +8,32 @@ import NoItem from '../NoItem'
 import { LinearProgress, Stack } from '@mui/material';
 
 function MyProducts() {
-    let store = useSelector((state) => { return state })
+  let store = useSelector((state) => { return state })
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
   const title = store.info.info.nickName;
   let nTitle = null;
   if (title.length > 8) {
     nTitle = title.substr(0, 8) + "...";
-} else {
+  } else {
     nTitle = title
-}
+  }
 
-console.log(store)
+  console.log(store)
   useEffect(() => {
     setLoading(true)
     async function fetchData() {
       const result = await axios.get(
         `/api/product/me`,
         {
-            headers: {
-              Authorization: `Bearer ${localStorage.token}`
-            }
+          headers: {
+            Authorization: `Bearer ${localStorage.token}`
           }
+        }
       );
       setData(result.data.reverse());
       setLoading(false)
-      }
+    }
     fetchData();
   }, []);
 
@@ -41,9 +41,9 @@ console.log(store)
   const Loading = () => {
     return (
       < >
-           <Stack sx={{ width: '100%', color: 'grey.500' }} spacing={2}>
-      <LinearProgress color="secondary" />
-    </Stack>
+        <Stack sx={{ width: '100%', color: 'grey.500' }} spacing={2}>
+          <LinearProgress color="secondary" />
+        </Stack>
       </>
     );
   };
@@ -54,56 +54,74 @@ console.log(store)
   const ShowProducts = () => {
     return (
       <>
-        { data && data.length > 0 ?
-        <>
-        {data.map((product) => {
-          let mainImg = null;
-          if (product.productImg.length>0) {
-            mainImg = 'https://i7c110.p.ssafy.io'+product.productImg[0].path
-          }
-          let pTitle = null;
-          if (product.title.length > 8){
-            pTitle = product.title.substr(0,8)+"...";
-          } else {
-            pTitle = product.title
-          }
-          
-          let price = product.price
-          const productPrice = price.toLocaleString('ko-KR');
+        {data && data.length > 0 ?
+          <>
+            {data.map((product) => {
+              let mainImg = null;
+              if (product.productImg.length > 0) {
+                mainImg = 'https://i7c110.p.ssafy.io' + product.productImg[0].path
+              }
+              let pTitle = null;
+              if (product.title.length > 8) {
+                pTitle = product.title.substr(0, 8) + "...";
+              } else {
+                pTitle = product.title
+              }
+
+              let price = product.price
+              const productPrice = price.toLocaleString('ko-KR');
 
 
-          return (
-            <li className='cards_item' key={product.no}>
-              <div className='card' style={{height: '250px'}}>
-                <a href={`/product/${product.no}`} >
-                      <img className='card_image'
-                        src={mainImg}
-                        alt={product.title}
-                        style={{height:'170px',width:'100%'}}
+              return (
+                <li className='cards_item' key={product.no}>
+                  <div className='card' style={{ height: '250px' }}>
+                    <a href={`/product/${product.no}`} >
+                      {product.state ?
+                        <div style={{ height: '170px' }}>
+                          <img className='card_image'
+                            src={mainImg}
+                            alt={product.title}
+                            style={{ opacity: '60%', width: '100%', height: '170px' }}
+                          />
+                          <h1 style={{ marginTop: '-100px', color: '#6667ab' }}>판매 완료</h1>
+                        </div>
+                        :
+                        <img className='card_image'
+                          src={mainImg}
+                          alt={product.title}
+                          style={{ height: '170px', width: '100%' }}
                         />
-                  <div className='card_content'>
-                    <h5 className='card_title'>{pTitle}</h5>
-                    <p className='card_text'>{productPrice} 원</p>
-                  </div>
+                      }
 
-                </a>
-              </div>
-            </li>
-            );
-          })}
-        </>
-        :
-        <NoItem></NoItem>
+
+
+
+
+
+
+                      <div className='card_content'>
+                        <h5 className='card_title'>{pTitle}</h5>
+                        <p className='card_text'>{productPrice} 원</p>
+                      </div>
+
+                    </a>
+                  </div>
+                </li>
+              );
+            })}
+          </>
+          :
+          <NoItem></NoItem>
         }
-        
+
       </>
     );
   };
 
   return (
     <>
-    <div className='fixnav'>
-      <NavBar />
+      <div className='fixnav'>
+        <NavBar />
       </div>
       <div className='content'>
         <h1>{nTitle}님의 쏠쏠상점</h1>
